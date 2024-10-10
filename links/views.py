7457de5a2bd1f387b5f545c9ff530d8c9eef0843
Andrei <andrei.baditoiu@gmail.com>
2024-10-10 22:52:57 +0300

    Added: index, _base(html), urls(py); modified: urls(config), admin, models, views(py).

--- links/views.py
+++ links/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Link
+
 
 # Create your views here.
+def index(request):
+    links = Link.objects.all()
+    context = {
+        'links': links
+    }
+    return render(request, 'index.html', context)
+
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click()
+    return redirect(link.url)
+
