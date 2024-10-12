c4909396a8deb8976fb81de201c07261464b4b5b
Andrei <andrei.baditoiu@gmail.com>
2024-10-12 23:44:12 +0300

    Modified: settings(.py), db, _base, create(.html).

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party packages
+    'crispy_forms',
+    'crispy_tailwind',
 
     # local apps
     'links',
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATES_PACKS = 'tailwind'
+
+CRISPY_TEMPLATE_PACK = 'tailwind'
