7ea719c59a3c56e2027a0da9f2e41df24d7919cd
Andrei <andrei.baditoiu@gmail.com>
2024-10-12 23:08:43 +0300

    Added forms(py); modified views(py), _base, create(html).

--- links/views.py
+++ links/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Link
+from .forms import LinkForm
 
 
 # Create your views here.
@@ -19,5 +20,15 @@ def root_link(request, link_slug):
 
 
 def add_link(request):
-    print(request.POST)
-    return render(request, 'create.html', {})
+    if request.method == "POST":
+        # form has data
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+
+        form = LinkForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'create.html', context)
